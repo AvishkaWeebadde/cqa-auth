0aedac368aca365e66a013c5ed43a4374ed86ed3
AvishkaWeebadde <senithweebadde@gmail.com>
2025-12-04 14:39:26 +0530

    feature:init auth service

---
+++ src/main/java/cqa/security/jwt/user/enumeration/Role.java
@@ -0,0 +1,6 @@
+package cqa.security.jwt.user.enumeration;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/cqa/security/jwt/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cqa.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
